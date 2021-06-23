b1ba432f72010cb4e912f2dd095fcf89d225a119
xTrM_ <xtrmyt24@gmail.com>
2021-06-23 17:49:48 +0200

    Construct list directly in constructor

--- src/main/java/fr/nkosmos/starboard/constraint/value/ChoiceConstraint.java
+++ src/main/java/fr/nkosmos/starboard/constraint/value/ChoiceConstraint.java
@@ -2,17 +2,22 @@ package fr.nkosmos.starboard.constraint.value;
 
 import fr.nkosmos.starboard.ISetting;
 import fr.nkosmos.starboard.constraint.ValueConstraint;
-import lombok.*;
+import lombok.Data;
 
 import java.util.Arrays;
+import java.util.List;
 
 public @Data class ChoiceConstraint<T> implements ValueConstraint<T> {
 
-    private final T[] choices;
+    private final List<T> choices;
+
+    public ChoiceConstraint(T... choices) {
+        this.choices = Arrays.asList(choices);
+    }
 
     @Override
     public T constraint(ISetting<T> setting, T value) {
-        if (Arrays.asList(choices).contains(value)) return value;
+        if (choices.contains(value)) return value;
         return setting.get();
     }
 
